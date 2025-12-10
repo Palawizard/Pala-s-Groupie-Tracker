8199685d45692bf98c4dcc1081e9e6c96d4670e5
Palawi <bapcoloj@gmail.com>
2025-12-10 15:33:00 +0100

    feat(css): added tailwind css for the style of the page
    usage : npm run dev:css

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,22 @@
-package server
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"palasgroupietracker/internal/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.HomeHandler)
+
+	fileServer := http.FileServer(http.Dir("web/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	log.Println("listening on :8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -1,1 +1,31 @@
 package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type HomePageData struct {
+	Title string
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(
+		"web/templates/layout.gohtml",
+		"web/templates/home.gohtml",
+	)
+	if err != nil {
+		http.Error(w, "template error", http.StatusInternalServerError)
+		return
+	}
+
+	data := HomePageData{
+		Title: "Groupie Tracker",
+	}
+
+	err = tmpl.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		http.Error(w, "render error", http.StatusInternalServerError)
+		return
+	}
+}
